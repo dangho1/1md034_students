2dab24f8fc4b974d17bdb6f1c7865a1133532018
dagh3068 <dagh3068@student.uu.se>
2020-02-01 14:55:13 +0100

    Added menuItem constructors and more in js_script

---
+++ public/js/js_script.js
@@ -0,0 +1,23 @@
+function menuItem(desc_1, desc_2, desc_3, imagePath) {
+    this.desc_1 = desc_1;
+    this.desc_2 = desc_2;
+    this.desc_3 = desc_3;
+}
+
+function returnItem(menuItem) {
+    return menuItem.desc_1 + ', ' + menuItem.desc_2;
+}
+
+
+let fire_burger = new menuItem("Not advised to eat on a date", "May cause side effects");
+let hej_burger = new menuItem("Will sing you a song on your birthday", "Too cute to actually eat", "hej");
+let uber_burger = new menuItem("Cheat meal godsent", "Questionable cheese", "May cause anxiety after consumption");
+
+let lettuce_burger = new menuItem("Plain and boring", "Might as well have a picadeli salad");
+let void_burger = new menuItem("Imaginary burger", "Seeing is not believing");
+
+let return_fire = returnItem(fire_burger);
+let return_hej = returnItem(hej_burger);
+
+console.log(return_fire);
+console.log(return_hej);
