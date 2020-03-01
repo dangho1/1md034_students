9f1e42b8f70bd5bcb6480f03be0c6620d93d8cff
dangho1 <dangho98@gmail.com>
2020-03-01 22:55:21 +0100

    messaging first task

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -21,12 +21,10 @@ const info = new Vue({
 	isHidden: true,
 	name: '',
 	email: '',
-	street: '',
-	house: '',
 	payment: '',
 	gender: '',
 	info: [],
-	infoDesc: ["Burger:", "Name:", "Email:", "Street: ", "House: ", "Payment: ", "Gender: "]
+	infoDesc: ["Burger:", "Name:", "Email:", "Payment: ", "Gender: "]
 	
       },
   
@@ -35,12 +33,78 @@ const info = new Vue({
 	
 	markDone: function() {
 		this.isHidden = false;
-		this.info.push(this.name, this.email, this.street, this.house,   this.payment, this.gender);
-		//return(this.info);
+		this.info.push(this.name, this.email, this.payment, this.gender);
+		return(this.info);
 	}
-}
-  
-    
-   
+}   
 });
+
+const orderButton = new Vue({
+	el: '#order_button',
+	data: {
+		customer_info: info.infoDesc,
+		hidden: true,
+		order: []
+	},
+	methods: {
+		finishOrder: function() {
+			this.hidden = false;
+			this.order = info.markDone();
+		}
+	}
+})
+
+'use strict';
+const socket = io();
+
+const deli = new Vue({
+  el: '#dots',
+  data: {
+    orders: {},
+  },
+  created: function () {
+    /* When the page is loaded, get the current orders stored on the server (in app.js) */
+    socket.on('initialize', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+
+    /* Whenever an addOrder is emitted by a client (every open map.html is a client),
+     * the server responds with a currentQueue message (this is defined in app.js).
+     * The message's data payload is the entire updated order object. Here we define what
+     * the client should do with it.
+     * Spoiler: We replace the current local order object with the new one. */
+    socket.on('currentQueue', function (data) {
+      this.orders = data.orders;
+    }.bind(this));
+  },
+  methods: {
+    getNext: function () {
+      /* This function returns the next available key (order number) in the orders object,
+       * it works under the assumptions that all keys are integers. */
+      let lastOrder = Object.keys(this.orders).reduce(function (last, next) {
+        return Math.max(last, next);
+      }, 0);
+      return lastOrder + 1;
+    },
+    addOrder: function (event) {
+      /* When you click in the map, a click event object is sent as parameter
+       * to the function designated in v-on:click (i.e. this one).
+       * The click event object contains among other things different coordinates 
+       * that we need when calculating where in the map the click actually happened. */
+      let offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top
+      };
+      socket.emit("addOrder", { 
+        orderId: this.getNext(),
+        details: { 
+          x: event.clientX - 10 - offset.x,
+          y: event.clientY - 10 - offset.y 
+        },
+        orderItems: ["Beans", "Curry"]
+      });
+    }
+  }
+});
+
 
